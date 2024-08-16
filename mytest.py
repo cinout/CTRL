4000e6a003cb437c79c6f09b4200e1a05f1faced
seaskyho <seaskyho@gmail.com>
2024-08-16 23:04:41 +1000

    update

--- mytest.py
+++ mytest.py
@@ -7,5 +7,7 @@ target_class = 4
 bs = 10
 data = torch.randint(0, 20, (bs, 5))
 print(data)
-data = data[torch.tensor([3, 1, 5])]
+indices = torch.tensor([3, 1, 5])
+print(indices.dtype)
+data = data[indices]
 print(data)

0ccd31560a3de8907e822f07f91e7fb3c8e84fb9
seaskyho <seaskyho@gmail.com>
2024-09-18 19:11:50 +1000

    update

--- runlocal.sh
+++ runlocal.sh
@@ -27,10 +27,11 @@ python -u main_train.py \
     \
     --pretrained_ssl_model "./Experiments/20240906_183247_61_43-cifar10-simclr-resnet18-poi0.01-magtrain50.0-magval100.0-bs512-lr0.06-knnfreq5-SSDYes/last.pth.tar" \
     --pretrained_linear_model "./Experiments/20240906_183247_61_43-cifar10-simclr-resnet18-poi0.01-magtrain50.0-magval100.0-bs512-lr0.06-knnfreq5-SSDYes/linear.pth.tar" \
+    --pretrained_frequency_model "" \
     \
     --minority_criterion ss_score \
     \
     --use_frequency_detector \
-    --frequency_detector_epochs 10 \
+    --frequency_detector_epochs 50 \
     \
     --note "use frequency detector" \

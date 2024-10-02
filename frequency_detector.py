2a98011244be759bcf68c28754a6a4f90492f11b
seaskyho <seaskyho@gmail.com>
2024-10-02 17:02:31 +1000

    update

--- frequency_detector.py
+++ frequency_detector.py
@@ -489,34 +489,34 @@ def patching_train(
     clean_sample.shape: [32, 32, 3]; value range: [0, 1]
     """
     # TODO: update attack range
-    attack = np.random.choice([5, 6, 7], 1)[0]
-    # if ensemble_id == 0:
-    #     if frequency_train_trigger_size == 2:
-    #         attack = np.random.choice([0, 1], 1)[0]
-    #     elif frequency_train_trigger_size == 3:
-    #         attack = np.random.choice([0, 1, 2], 1)[0]
-    #     elif frequency_train_trigger_size == 4:
-    #         attack = np.random.choice([0, 1, 2, 3], 1)[0]
-    #     elif frequency_train_trigger_size == 5:
-    #         attack = np.random.choice([0, 1, 2, 3, 4], 1)[0]
-    # elif ensemble_id == 1:
-    #     if frequency_train_trigger_size == 2:
-    #         attack = np.random.choice([2, 3], 1)[0]
-    #     elif frequency_train_trigger_size == 3:
-    #         attack = np.random.choice([2, 3, 4], 1)[0]
-    #     elif frequency_train_trigger_size == 4:
-    #         attack = np.random.choice([1, 2, 3, 4], 1)[0]
-    #     elif frequency_train_trigger_size == 5:
-    #         attack = np.random.choice([0, 1, 2, 3, 4], 1)[0]
-    # elif ensemble_id == 2:
-    #     if frequency_train_trigger_size == 2:
-    #         attack = np.random.choice([1, 4], 1)[0]
-    #     elif frequency_train_trigger_size == 3:
-    #         attack = np.random.choice([0, 3, 4], 1)[0]
-    #     elif frequency_train_trigger_size == 4:
-    #         attack = np.random.choice([0, 1, 2, 4], 1)[0]
-    #     elif frequency_train_trigger_size == 5:
-    #         attack = np.random.choice([0, 1, 2, 3, 4], 1)[0]
+
+    if ensemble_id == 0:
+        # if frequency_train_trigger_size == 2:
+        #     attack = np.random.choice([0, 1], 1)[0]
+        # elif frequency_train_trigger_size == 3:
+        #     attack = np.random.choice([0, 1, 2], 1)[0]
+        # elif frequency_train_trigger_size == 4:
+        #     attack = np.random.choice([0, 1, 2, 3], 1)[0]
+        # elif frequency_train_trigger_size == 5:
+        attack = np.random.choice([0, 1, 2, 3, 4], 1)[0]
+    elif ensemble_id == 1:
+        # if frequency_train_trigger_size == 2:
+        #     attack = np.random.choice([2, 3], 1)[0]
+        # elif frequency_train_trigger_size == 3:
+        #     attack = np.random.choice([2, 3, 4], 1)[0]
+        # elif frequency_train_trigger_size == 4:
+        #     attack = np.random.choice([1, 2, 3, 4], 1)[0]
+        # elif frequency_train_trigger_size == 5:
+        attack = np.random.choice([5, 6, 7, 1, 2], 1)[0]
+    elif ensemble_id == 2:
+        # if frequency_train_trigger_size == 2:
+        #     attack = np.random.choice([1, 4], 1)[0]
+        # elif frequency_train_trigger_size == 3:
+        #     attack = np.random.choice([0, 3, 4], 1)[0]
+        # elif frequency_train_trigger_size == 4:
+        #     attack = np.random.choice([0, 1, 2, 4], 1)[0]
+        # elif frequency_train_trigger_size == 5:
+        attack = np.random.choice([3, 4, 5, 6, 7], 1)[0]
 
     pat_size_x = np.random.randint(2, 8)
     pat_size_y = np.random.randint(2, 8)

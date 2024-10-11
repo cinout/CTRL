538d3b6ed6e85aa199d96327274933712a8fcb07
seaskyho <seaskyho@gmail.com>
2024-10-11 23:09:22 +1100

    update

--- methods/base.py
+++ methods/base.py
@@ -134,8 +134,8 @@ def get_ss_statistics(
 
         # scaler = MinMaxScaler()
         clusters = KMeans(
-            n_clusters=args.knn_cluster_num, n_init="auto", init="k-means++"
-        ).fit(visual_features / np.linalg.norm(visual_features, axis=-1, keepdims=True))
+            n_clusters=args.knn_cluster_num, n_init=30, init="k-means++"
+        ).fit(visual_features)
         labels = clusters.labels_
 
         corrs_total = np.zeros(shape=(1, bs), dtype=visual_features.dtype)

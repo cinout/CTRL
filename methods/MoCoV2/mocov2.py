0324901ce4d89b3147872663328da2c41e886c22
seaskyho <seaskyho@gmail.com>
2024-11-19 22:00:59 +1100

    update

--- methods/MoCoV2/mocov2.py
+++ methods/MoCoV2/mocov2.py
@@ -127,15 +127,15 @@ class MoCo(nn.Module):
         # replace the keys at ptr (dequeue and enqueue)
 
         if ptr + batch_size > self.K:
-            print(f"<<<<< ptr: {ptr}")
-            print(f"<<<<< batch_size: {batch_size}")
-            print(f"<<<<< self.K: {self.K}")
-            print(f"<<<<< self.queue.shape: {self.queue.shape}")
-            print(f"<<<<< keys.T.shape: {keys.T.shape}")
-            print(f"<<<<<<<<<<<<<<<")
+            # print(f"<<<<< ptr: {ptr}")
+            # print(f"<<<<< batch_size: {batch_size}")
+            # print(f"<<<<< self.K: {self.K}")
+            # print(f"<<<<< self.queue.shape: {self.queue.shape}")
+            # print(f"<<<<< keys.T.shape: {keys.T.shape}")
+            # print(f"<<<<<<<<<<<<<<<")
 
             self.queue[:, ptr : self.K] = keys.T[:, : self.K - ptr]
-            self.queue[: ptr + batch_size - self.K] = keys.T[:, self.K - ptr :]
+            self.queue[:, : ptr + batch_size - self.K] = keys.T[:, self.K - ptr :]
         else:
             self.queue[:, ptr : ptr + batch_size] = keys.T
 

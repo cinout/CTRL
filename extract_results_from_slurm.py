3ad7deb52e86f6ebfb7a9c4dd9091f3fa1038c72
seaskyho <seaskyho@gmail.com>
2025-07-19 17:14:15 +1000

    update

--- extract_results_from_slurm.py
+++ extract_results_from_slurm.py
@@ -56,31 +56,31 @@ Input File Paths
 """
 # FIXME: change
 all_input_file_paths = [
-    "slurm-13015371-[ST:vote4_rm30-120_sd30].out",
-    "slurm-13015372.out",
-    "slurm-13015373.out",
-    "slurm-13015374.out",
-    "slurm-13015375.out",
-    "slurm-13015376.out",
-    "slurm-13015377.out",
-    "slurm-13015378.out",
-    "slurm-13015379.out",
-    "slurm-13015380.out",
-    "slurm-13015381.out",
-    "slurm-13015382.out",
-    "slurm-13015383.out",
-    "slurm-13015384.out",
-    "slurm-13015385.out",
-    "slurm-13015386.out",
-    "slurm-13015387.out",
-    "slurm-13015388-[END:vote4_rm30-120_sd30].out",
+    "slurm-13016303-[ST:vote4_rm30-120_sd42].out",
+    "slurm-13016304.out",
+    "slurm-13016305.out",
+    "slurm-13016306.out",
+    "slurm-13016307.out",
+    "slurm-13016308.out",
+    "slurm-13016309.out",
+    "slurm-13016310.out",
+    "slurm-13016311.out",
+    "slurm-13016312.out",
+    "slurm-13016313.out",
+    "slurm-13016314.out",
+    "slurm-13016315.out",
+    "slurm-13016316.out",
+    "slurm-13016317.out",
+    "slurm-13016318.out",
+    "slurm-13016319.out",
+    "slurm-13016320-[END:vote4_rm30-120_sd42].out",
 ]
 
 """
 Output File Paths
 """
 prefix = "zz_results_"
-title = "vote4_rm30-120_sd30_"  # FIXME: change
+title = "vote4_rm30-120_sd42_"  # FIXME: change
 output_file_voted_channels = prefix + title + "voted_channels.py"
 output_file_acc_asr = prefix + title + "acc_asr_results.txt"
 

f831733444a2469442be1d7a4d126841ff5bc3b7
seaskyho <seaskyho@gmail.com>
2025-07-28 11:10:49 +1000

    update

--- extract_multiple_results_from_slurm.py
+++ extract_multiple_results_from_slurm.py
@@ -96,7 +96,7 @@ all_input_file_paths = [
 Output File Paths
 """
 prefix = "zz_results_"
-title = "sample_4_sd20_"  # FIXME: change
+title = "sample_6_sd20_"  # FIXME: change
 output_file_voted_channels = prefix + title + "voted_channels.py"
 output_file_acc_asr = prefix + title + "acc_asr_results.txt"
 

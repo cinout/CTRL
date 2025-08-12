c3520be19ce4f6ff86739821086106075e0e8a0f
seaskyho <seaskyho@gmail.com>
2025-08-13 09:15:16 +1000

    update

--- main_train.py
+++ main_train.py
@@ -750,7 +750,7 @@ if __name__ == "__main__":
 
     # Defense Baseline: SSL-Cleanse generated triggers
     if args.trigger_path == "":
-        args.trigger_path = f"{args.timestamp}_trigger_estimation_{args.method}_{args.dataset}_{args.trigger_type}_SD{args.ssl_pretrain_seed}"
+        args.trigger_path = f"{args.timestamp}_trigger_estimation_{args.method}_{args.dataset}_{args.trigger_type}_SD{args.ssl_cleanse_seed}"
 
     if not os.path.exists(args.saved_path):
         os.makedirs(args.saved_path)

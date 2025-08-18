bf4a8bc420fe744710ff820b4d62fc68c2b9dba5
seaskyho <seaskyho@gmail.com>
2025-08-18 11:24:57 +1000

    update

--- compute_reduction_rate.py
+++ compute_reduction_rate.py
@@ -1,19 +1,108 @@
-from collections import Counter
 import numpy as np
-import torch
-import random
 
+"""
+ACC
+"""
+uncleansed_acc = [
+    # BYOL
+    42.7,
+    86.9,
+    55.9,
+    42.8,
+    88,
+    56.6,
+    22.2,
+    85.0,
+    34.9,
+    22.3,
+    77.3,
+    36.2,
+    # MoCo
+    42.4,
+    78.1,
+    45.5,
+    42.5,
+    79.3,
+    45.5,
+    26,
+    69.5,
+    24.5,
+    26.7,
+    70.6,
+    27.2,
+    # SimCLR
+    38.1,
+    84.9,
+    48.8,
+    38.3,
+    85,
+    49.7,
+    21.6,
+    73,
+    25,
+    24.6,
+    75.7,
+    24.8,
+]
+baseline_acc = [
+    # BYOL
+    24.5,
+    86.7,
+    56.0,
+    23.4,
+    87.2,
+    56.0,
+    17.4,
+    82.5,
+    44.2,
+    16.2,
+    83.6,
+    44.3,
+    # MoCo
+    31.4,
+    76.8,
+    43.8,
+    30.5,
+    77.7,
+    43.6,
+    23.6,
+    71.3,
+    28.0,
+    22.4,
+    72.8,
+    29.1,
+    # SimCLR
+    30.4,
+    87.2,
+    53.9,
+    32.0,
+    87.1,
+    53.4,
+    23.4,
+    84.7,
+    42.1,
+    24.5,
+    83.6,
+    42.4,
+]
+uncleansed_acc = np.array(uncleansed_acc)
+baseline_acc = np.array(baseline_acc)
+baseline_percent_wrt_uncleansed = (baseline_acc - uncleansed_acc) / uncleansed_acc
+print(f"ACC reduction rate:{baseline_percent_wrt_uncleansed.mean()*100:.1f}")
 
+"""
+ASR
+"""
 uncleansed_asr = [
     # BYOL
     2.1,
-    4.1,
+    67.7,
     44.0,
     41.9,
     83.0,
     88.0,
     0.0,
-    2.0,
+    94.6,
     50.6,
     62.7,
     89.9,
@@ -49,104 +138,66 @@ uncleansed_asr = [
 
 baseline_asr = [
     # BYOL
-    1.9,
-    2.1,
+    1.5,
     2.2,
-    2.1,
-    6.5,
-    13.5,
+    1.4,
+    1.4,
+    8.3,
+    14.5,
     1.6,
-    5.7,
-    3.1,
-    2.4,
-    26.0,
-    6.2,
+    19.8,
+    1.8,
+    1.7,
+    17.2,
+    6.6,
     # MoCo
+    2.7,
+    23.5,
+    22.0,
     1.8,
-    23.9,
-    20.1,
-    2.2,
-    2.4,
+    2.3,
     0.6,
-    2.6,
-    15.8,
-    20.6,
-    1.8,
-    3.7,
-    1.0,
+    4.9,
+    24.4,
+    17.1,
+    1.4,
+    2.9,
+    0.8,
     # SimCLR
-    1.8,
-    23.9,
-    20.1,
-    2.2,
+    4.1,
+    47.4,
+    29.0,
     2.4,
-    0.6,
-    2.6,
-    15.8,
-    20.6,
-    1.8,
-    3.7,
-    1.0,
-]
-
-
-ours_sample16_asr = [
-    # BYOL
-    0.4,
-    4.5,
-    0.0,
-    0.8,
-    37.8,
-    0.1,
-    0.3,
-    7.6,
-    0.0,
-    0.7,
-    14.3,
-    0.3,
-    # MoCo
-    2.4,
-    53.7,
-    3.5,
-    0.8,
-    1.1,
-    0.1,
-    0.3,
-    56.7,
-    1.2,
-    0.7,
-    2.1,
-    0.3,
-    # SimCLR
-    1.3,
-    0.8,
-    0.0,
-    0.4,
-    14.6,
-    0.0,
-    0.0,
-    0.1,
-    0.0,
-    0.0,
-    3.1,
     12.0,
+    16.7,
+    1.7,
+    45.5,
+    24.7,
+    2.4,
+    11.9,
+    11.9,
 ]
+
+
+# ours_sample16_asr = [
+#     #
+# ]
 
 uncleansed_asr = np.array(uncleansed_asr)
 baseline_asr = np.array(baseline_asr)
-ours_sample16_asr = np.array(ours_sample16_asr)
+# ours_sample16_asr = np.array(ours_sample16_asr)
 
-"""
-Absolute Diff
-"""
+# """
+# Absolute Diff
+# """
 
-diff = baseline_asr - ours_sample16_asr
+# diff = baseline_asr - ours_sample16_asr
 
-baseline_wins = np.sum(diff < 0)
-our_wins = np.sum(diff > 0)
-ties = np.sum(diff == 0)
+# baseline_wins = np.sum(diff < 0)
+# our_wins = np.sum(diff > 0)
+# ties = np.sum(diff == 0)
 
-print(f"baseline_wins: {baseline_wins}; our_wins: {our_wins}; ties: {ties}")
+# print(f"baseline_wins: {baseline_wins}; our_wins: {our_wins}; ties: {ties}")
 
 
 """
@@ -157,15 +208,13 @@ uncleansed_asr_gt_1 = uncleansed_asr > 1
 
 uncleansed_asr = uncleansed_asr[uncleansed_asr_gt_1]
 baseline_asr = baseline_asr[uncleansed_asr_gt_1]
-ours_sample16_asr = ours_sample16_asr[uncleansed_asr_gt_1]
+# ours_sample16_asr = ours_sample16_asr[uncleansed_asr_gt_1]
 
 
 baseline_percent_wrt_uncleansed = (baseline_asr - uncleansed_asr) / uncleansed_asr
-ours_sample16_percent_wrt_uncleansed = (
-    ours_sample16_asr - uncleansed_asr
-) / uncleansed_asr
+# ours_sample16_percent_wrt_uncleansed = (
+#     ours_sample16_asr - uncleansed_asr
+# ) / uncleansed_asr
 
 
-print(
-    f"baseline reduction rate:{baseline_percent_wrt_uncleansed.mean()}, our reduction rate: {ours_sample16_percent_wrt_uncleansed.mean()}"
-)
+print(f"ASR reduction rate:{baseline_percent_wrt_uncleansed.mean()*100:.1f}")

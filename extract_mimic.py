856960ac3f83a2628a71390b4e34243d66779796
seaskyho <seaskyho@gmail.com>
2025-08-22 10:11:16 +1000

    update

--- extract_mimic.py
+++ extract_mimic.py
@@ -57,31 +57,31 @@ Input File Paths
 """
 # FIXME: change
 all_input_file_paths = [
-    "slurm-14680931-[ST:mimic_sd10].out",
-    "slurm-14680932.out",
-    "slurm-14680933.out",
-    "slurm-14680934.out",
-    "slurm-14680935.out",
-    "slurm-14680936.out",
-    "slurm-14680937.out",
-    "slurm-14680938.out",
-    "slurm-14680939.out",
-    "slurm-14680940.out",
-    "slurm-14680941.out",
-    "slurm-14680942.out",
-    "slurm-14680943.out",
-    "slurm-14680944.out",
-    "slurm-14680945.out",
-    "slurm-14680946.out",
-    "slurm-14680947.out",
-    "slurm-14680948-[END:mimic_sd10].out",
+    "slurm-14681146-[ST:mimic_sd42].out",
+    "slurm-14681147.out",
+    "slurm-14681148.out",
+    "slurm-14681149.out",
+    "slurm-14681150.out",
+    "slurm-14681151.out",
+    "slurm-14681152.out",
+    "slurm-14681153.out",
+    "slurm-14681154.out",
+    "slurm-14681155.out",
+    "slurm-14681156.out",
+    "slurm-14681157.out",
+    "slurm-14681158.out",
+    "slurm-14681159.out",
+    "slurm-14681160.out",
+    "slurm-14681161.out",
+    "slurm-14681162.out",
+    "slurm-14681163-[END:mimic_sd42].out",
 ]
 
 """
 Output File Paths
 """
 prefix = "zz_results_"
-title = "mimic_sd10_"  # FIXME: change
+title = "mimic_sd42_"  # FIXME: change
 output_file_voted_channels = prefix + title + "voted_channels.py"
 output_file_acc_asr = prefix + title + "acc_asr_results.txt"
 

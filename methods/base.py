1b143a2ca949c47f1edde6ac43cc17801c1cd958
seaskyho <seaskyho@gmail.com>
2025-08-29 18:03:28 +1000

    update

--- methods/base.py
+++ methods/base.py
@@ -525,6 +525,8 @@ def find_trigger_channels_or_poisoned_images(
                 clean_indices, args.find_channels_from_n_clean_samples
             )
         all_indices = [*poisoned_indices, *clean_indices]
+        print(f"poisoned_indices size: ", len(poisoned_indices))
+        print(f"clean_indices size: ", len(clean_indices))
         print(f"all_indices size: ", len(all_indices))
 
         subset = Subset(dataset, all_indices)

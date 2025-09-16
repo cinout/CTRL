ad82bac55289574901e2fc58751f84bbbee3e80c
seaskyho <seaskyho@gmail.com>
2025-09-16 23:18:23 +1000

    update

--- draw_tsne_plot.py
+++ draw_tsne_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-vision_features = np.load("visions_for_tsne_imagenet100_ftrojan_simclr.npy")
+vision_features = np.load("visions_for_tsne_imagenet100_ftrojan_simclr_2class.npy")
 
 
 bs, n_views, C = vision_features.shape
@@ -23,29 +23,51 @@ X_2d = tsne.fit_transform(X_flat)  # shape [n*views, 2]
 # Prepare labels: same label for all views of a class
 labels = np.repeat(np.arange(bs), n_views)  # shape [n*views]
 
-# 6 classes
-legends = {
-    0: "Class 1 Clean",
-    1: "Class 2 Clean",
-    2: "Class 3 Clean",
-    3: "Class 4 Clean",
-    4: "Class 5 Clean",
-    5: "Class 6 Clean",
-    6: "Class 1 Poison",
-    7: "Class 2 Poison",
-    8: "Class 3 Poison",
-    9: "Class 4 Poison",
-    10: "Class 5 Poison",
-    11: "Class 6 Poison",
-}
-
-# 2 classes
+# # 6 classes
 # legends = {
 #     0: "Class 1 Clean",
 #     1: "Class 2 Clean",
-#     2: "Class 1 Poison",
-#     3: "Class 2 Poison",
+#     2: "Class 3 Clean",
+#     3: "Class 4 Clean",
+#     4: "Class 5 Clean",
+#     5: "Class 6 Clean",
+#     6: "Class 1 Poison",
+#     7: "Class 2 Poison",
+#     8: "Class 3 Poison",
+#     9: "Class 4 Poison",
+#     10: "Class 5 Poison",
+#     11: "Class 6 Poison",
 # }
+
+# colors = {
+#     0: "red",
+#     1: "blue",
+#     2: "green",
+#     3: "orange",
+#     4: "purple",
+#     5: "cyan",
+#     6: "black",
+#     7: "black",
+#     8: "black",
+#     9: "black",
+#     10: "black",
+#     11: "black",
+# }
+
+# 2 classes
+legends = {
+    0: "Class 1 Clean",
+    1: "Class 2 Clean",
+    2: "Class 1 Poison",
+    3: "Class 2 Poison",
+}
+
+colors = {
+    0: "orange",
+    1: "purple",
+    2: "black",
+    3: "black",
+}
 
 # Plot
 plt.figure(figsize=(8, 6))
@@ -53,7 +75,7 @@ for i in range(bs):
     print(i)
     idx = labels == i
     plt.scatter(
-        X_2d[idx, 0], X_2d[idx, 1], label=legends[i], s=50
+        X_2d[idx, 0], X_2d[idx, 1], label=legends[i], s=30, c=colors[i]
     )  # all views same color
 
 plt.legend()
--- methods/base.py
+++ methods/base.py
@@ -1905,22 +1905,33 @@ class CLTrainer:
                 clean_val_dataset = poison.test_clean_loader.dataset
                 poi_val_dataset = poison.test_pos_loader.dataset
 
-                #  ensure two different classes
+                #  2 classes
                 index_1 = 2
-                index_2 = 600
-                index_3 = 1509
-                index_4 = 2006
-                index_5 = 3002
                 index_6 = 4001
-
                 clean_subset = Subset(
                     clean_val_dataset,
-                    [index_1, index_2, index_3, index_4, index_5, index_6],
+                    [index_1, index_6],
                 )
                 poi_subset = Subset(
                     poi_val_dataset,
-                    [index_1, index_2, index_3, index_4, index_5, index_6],
+                    [index_1, index_6],
                 )
+
+                # #  6 classes
+                # index_1 = 2
+                # index_2 = 600
+                # index_3 = 1509
+                # index_4 = 2006
+                # index_5 = 3002
+                # index_6 = 4001
+                # clean_subset = Subset(
+                #     clean_val_dataset,
+                #     [index_1, index_2, index_3, index_4, index_5, index_6],
+                # )
+                # poi_subset = Subset(
+                #     poi_val_dataset,
+                #     [index_1, index_2, index_3, index_4, index_5, index_6],
+                # )
 
                 clean_images = torch.stack([item[0] for item in clean_subset], dim=0)
                 poi_images = torch.stack([item[0] for item in poi_subset], dim=0)
